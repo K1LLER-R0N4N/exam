d5e9ed17e77865e668c0e972fca79b3a7a6a62a8
K1LLER-R0N4N <138888767+K1LLER-R0N4N@users.noreply.github.com>
2023-08-31 22:18:32 +0500

    Delete exam_ directory

--- exam_/home_page/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import BlogPost
-
-
-class BlogPostAdmin(admin.ModelAdmin):
-  list_display = ("title", "content", "pub_date",)
-  
-admin.site.register(BlogPost, BlogPostAdmin)
